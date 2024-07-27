c3d93157a9877dbf45b628255fd82b295c72fc7c
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-07-28 00:19:34 +0700

    Add files via upload

---
+++ kill.sh
@@ -0,0 +1,21 @@
+source $FUNCTION
+echo ""
+status=$(pgrep -f hnx) >/dev/null 2>&1
+if [ "$status" ]; then
+  pkill -f gsctx32 >/dev/null 2>&1
+  pkill -f gsctx64 >/dev/null 2>&1
+  rm /data/local/tmp/gsctx32 >/dev/null 2>&1
+  rm /data/local/tmp/gsctx64 >/dev/null 2>&1
+fi
+
+status=$(pgrep -f hnx) >/dev/null 2>&1
+if [ ! "$status" ]; then
+  echo ""
+  echo " Goodsettings stopped in background."
+  echo ""
+else
+  echo ""
+  echo " Goodsettings failed to stop."
+  echo ""
+fi
+echo ""
