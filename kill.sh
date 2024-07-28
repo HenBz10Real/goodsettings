23ca99f58469aedd63d3feb0ee5cbf2457123410
hnxsettings@1 <141293896+HenBz10Real@users.noreply.github.com>
2024-07-28 12:48:16 +0700

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,14 +1,20 @@
 source $FUNCTION
 echo ""
-status=$(pgrep -f hnx) >/dev/null 2>&1
+status=$(pgrep -f gsctx32) >/dev/null 2>&1
 if [ "$status" ]; then
   pkill -f gsctx32 
+  rm /data/local/tmp/gsctx32 >/dev/null 2>&1
+  rm /data/local/tmp/gsctx64 >/dev/null 2>&1
+fi
+
+status=$(pgrep -f gsctx64) >/dev/null 2>&1
+if [ "$status" ]; then
   pkill -f gsctx64
   rm /data/local/tmp/gsctx32 >/dev/null 2>&1
   rm /data/local/tmp/gsctx64 >/dev/null 2>&1
 fi
 
-status=$(pgrep -f hnx) >/dev/null 2>&1
+status=$(pgrep -f gsctx32) >/dev/null 2>&1
 if [ ! "$status" ]; then
   echo ""
   echo " Goodsettings stopped in background."
