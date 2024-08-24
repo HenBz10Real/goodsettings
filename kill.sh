fa84e0ef187125bb6d69a91b322d7a696658e416
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-25 02:39:38 +0700

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,20 +1,13 @@
 $AXFUN
 echo ""
-status=$(pgrep -f gsctx32) >/dev/null 2>&1
-if [ "$status" ]; then
-  pkill -f gsctx32 
-  rm /data/local/tmp/gsctx32 >/dev/null 2>&1
-  rm /data/local/tmp/gsctx64 >/dev/null 2>&1
-fi
-
-status=$(pgrep -f gsctx64) >/dev/null 2>&1
+status=$(pgrep -f gsctx) >/dev/null 2>&1
 if [ "$status" ]; then
   pkill -f gsctx64
   rm /data/local/tmp/gsctx32 >/dev/null 2>&1
   rm /data/local/tmp/gsctx64 >/dev/null 2>&1
 fi
 
-status=$(pgrep -f gsctx32) >/dev/null 2>&1
+status=$(pgrep -f gsctx) >/dev/null 2>&1
 if [ ! "$status" ]; then
   echo ""
   echo " Goodsettings stopped in background."
