c82fb9d2d99299dba6df3f3b9c528da8ad85b851
henpexvz <141293896+HenBz10Real@users.noreply.github.com>
2024-08-25 11:57:32 +0700

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -3,8 +3,6 @@ echo ""
 status=$(pgrep -f gsctx) >/dev/null 2>&1
 if [ "$status" ]; then
   pkill -f gsctx
-  rm /data/local/tmp/gsctx32 >/dev/null 2>&1
-  rm /data/local/tmp/gsctx64 >/dev/null 2>&1
 fi
 
 status=$(pgrep -f gsctx) >/dev/null 2>&1
